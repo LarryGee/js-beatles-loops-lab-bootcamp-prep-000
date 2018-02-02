5d7e50119787d190120ae188414e45003fa614c7
Larry Jr <larry.g3284@yahoo.com>
2018-02-02 23:57:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ var allPlayers = [];
   return allPlayers;
 }
 
-function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  var newFacts = [];
-  var i = 0;
-  while (newFacts.length < facts.length) {
-    newFacts.push(facts[i] + "!!!");
-    i++;
+function johnLennonFacts(facts) {
+  var results = [];
+  var count = 0;
+  while (count < facts.length) {
+    results.push(facts[count] + "!!!");
+    count++;
   }
-  return newFacts;
+  return results;
 }
 
 function iLoveTheBeatles(num) {
